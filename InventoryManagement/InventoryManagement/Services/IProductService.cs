c30144bea3fc574052f386548eff46883e4015db
Pankaj78172 <pankajpaliwal61@gmail.com>
2026-04-16 15:35:06 -0600

    working on service need to finish that up

---
+++ InventoryManagement/InventoryManagement/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using InventoryManagement.DTO;
+
+namespace InventoryManagement.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductReadDto>> GetAllProductsAsync();
+        Task<ProductReadDto?> GetProductByIdAsync(int id);
+        Task<ProductReadDto> CreateProductAsync(ProductCreateDTO dto);
+        Task<bool> UpdateProductAsync(int id, ProductUpdateDto dto);
+        Task<bool> DeleteProductAsync(int id);
+        Task<IEnumerable<ProductReadDto>> GetProductsByCategoryAsync(string category);
+        Task<IEnumerable<ProductReadDto>> GetLowStockProductsAsync();
+        Task<IEnumerable<ProductReadDto>> SearchProductsAsync(string name);
+
+
+
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Services/ProductService.cs
@@ -0,0 +1,81 @@
+﻿using InventoryManagement.Data;
+using InventoryManagement.DTO;
+using InventoryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Services
+{
+    public class ProductService: IProductService
+    {
+        private readonly AppDbContext _context;
+
+        public ProductService(AppDbContext context) { 
+        _context = context;
+        }
+
+        public async Task<IEnumerable<ProductReadDto>> GetAllProductsAsync()
+        {
+            return await _context.Products
+                            .Select(p => new ProductReadDto
+                            {
+                                Id = p.Id,
+                                Name = p.Name,
+                                Category = p.Category,
+                                Price = p.Price,
+                                QuantityInStock = p.QuantityInStock,
+                                SupplierName = p.SupplierName,
+                                CreatedDate = p.CreatedDate
+                            }
+                ).ToListAsync();
+        }
+
+        public async Task<ProductReadDto?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Where(p => p.Id == id)
+                        .Select(x => new ProductReadDto {
+                            Id = x.Id,
+                            Name = x.Name,
+                            Category = x.Category,
+                            Price = x.Price,
+                            QuantityInStock = x.QuantityInStock,
+                            SupplierName = x.SupplierName,
+                            CreatedDate = x.CreatedDate
+                        }).FirstOrDefaultAsync();
+
+        }
+
+        public async Task<ProductReadDto> CreateProductAsync(ProductCreateDTO dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Category = dto.Category,
+                Price = dto.Price,
+                QuantityInStock = dto.QuantityInStock,
+                SupplierName = dto.SupplierName,
+                CreatedDate = DateTime.Now
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return new ProductReadDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Category = product.Category,
+                Price = product.Price,
+                QuantityInStock = product.QuantityInStock,
+                SupplierName = product.SupplierName,
+                CreatedDate = product.CreatedDate
+
+            };
+}
+
+
+
+
+
+    }
+}
